8671fd84d9b74c03a492b7fb9adc666aaa017a87
Vincent Hou <shou@us.ibm.com>
2016-07-12 16:38:45 -0400

    Add nodejs and nodejs 6 action tests for word count

--- tests/src/packages/samples/WordCountTest.scala
+++ tests/src/packages/samples/WordCountTest.scala
@@ -1,5 +1,6 @@
 package packages.samples
 
+import java.io.File
 import org.junit.runner.RunWith
 import org.scalatest.junit.JUnitRunner
 import scala.concurrent.duration.DurationInt
@@ -23,6 +24,8 @@ class WordCountTest extends TestHelpers
     implicit val wskprops = WskProps()
     val wsk = new Wsk(usePythonCLI=true)
     val wordcountAction = "/whisk.system/samples/wordCount"
+    var catalogDir = new File(scala.util.Properties.userDir.toString(), "../packages")
+    val sample_file = "samples/wordcount/javascript/wordcount.js"
 
     behavior of "samples wordCount"
 
@@ -49,6 +52,46 @@ class WordCountTest extends TestHelpers
                     activation.getFieldPath("response", "success") should be(Some(false.toJson))
                     activation.getFieldPath("response", "result", "error") should be(
                         Some(expectedError))
+            }
+    }
+
+    it should "Create an action, then return the number of words when sending the words as payload on Nodejs" in withAssetCleaner(wskprops) {
+        (wp, assetHelper) =>
+            val name = "wcNodejs"
+            val file = new File(catalogDir, sample_file).toString()
+     
+            assetHelper.withCleaner(wsk.action, name) {
+                (action, _) => action.create(name, Some(file), kind = Some("nodejs"))
+            }
+            wsk.action.get(name).stdout should include regex (""""kind": "nodejs"""")
+ 
+            val run = wsk.action.invoke(name, Map("payload" -> "Five fuzzy felines".toJson))
+            val expectedNumber = 3
+            withActivation(wsk.activation, run) {
+                activation =>
+                    activation.getFieldPath("response", "success") should be(Some(true.toJson))
+                    activation.getFieldPath("response", "result", "count").toString should be(
+                        Some(expectedNumber).toString)
+            }
+    }
+
+    it should "Create an action, then return the number of words when sending the words as payload on Nodejs 6" in withAssetCleaner(wskprops) {
+        (wp, assetHelper) =>
+            val name = "wcNodejs"
+            val file = new File(catalogDir, sample_file).toString()
+     
+            assetHelper.withCleaner(wsk.action, name) {
+                (action, _) => action.create(name, Some(file), kind = Some("nodejs:6"))
+            }
+            wsk.action.get(name).stdout should include regex (""""kind": "nodejs:6"""")
+ 
+            val run = wsk.action.invoke(name, Map("payload" -> "Five fuzzy felines".toJson))
+            val expectedNumber = 3
+            withActivation(wsk.activation, run) {
+                activation =>
+                    activation.getFieldPath("response", "success") should be(Some(true.toJson))
+                    activation.getFieldPath("response", "result", "count").toString should be(
+                        Some(expectedNumber).toString)
             }
     }
 }
